b5b1636b3fb6a0cf26c50831414245a8e571afdf
Michael Scott <michealgscarn@gmail.com>
2020-10-13 14:29:47 +0000

    Completed

--- Triangle.java
+++ Triangle.java
@@ -12,18 +12,20 @@ public class Triangle
 
 	public Triangle(int a, int b, int c)
 	{
-
+    sideA=a;
+    sideB=b;
+    sideC=c;
 	}
 
 	public int getPerimeter( )
 	{
-    return 0;
+    return sideA+sideB+sideC;
 	}
 
 	public double getArea( )
 	{
     double perimeter = getPerimeter( );
-
-		return 0;
+    double s=perimeter/2;
+		return Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));
 	}
 }
